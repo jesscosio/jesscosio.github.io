5ee50db3266f5ef41a35a177796a41cc21d4ad99
jess cosio <jessica.cosio@icloud.com>
2020-05-28 22:02:32 -0700

    editing about

--- src/about/aboutMe.vue
+++ src/about/aboutMe.vue
@@ -4,52 +4,52 @@
         <img id="aboutImg" data-src="../assets/heraldsquare.jpg" alt="heraldsquare">
         
          <div class="bio">
-            <p> Contact • EMAIL : jessica.cosio@icloud.com • MOBILE : (805) 865-6148 </p>
+            <p> EMAIL : jessica.cosio@icloud.com MOBILE : (805) 865-6148 </p>
             <h2> EDUCATION </h2><h3>CALIFORNIA POLYTECHNIC STATE UNIVERSITY SAN LUIS OBISPO, CA B.S. Software Engineering</h3>
             <h2> EXPERIENCE</h2> 
 <h3>Macy’s Inc. Software Engineer San Francisco, CA Aug 2015 - Present</h3>
             <p>
-<br>• Full stack web development – [javascript, html, css/scss, node, npm, webpack, vue, git, react, typescript, express, backbone, responsive]
-<br>• A/B testing and KPI tracking using web analytics implementations
-<br>• Analytics using IBM coremetrics
-<br>• High priority/urgent code delivery and defect fixes
-<br>• Unit test coverage and performance test base lining behind delivered features 
-<br>• Strong emphasis on process and delivery- especially using lean principles
-<br>• Web app UX research - established a/b test result implications, and proceeded with implementation 
-<br>• Daily documentation of work as a steady habit
-<br>• Weekly showcase of work to business stakeholders
+<br> Full stack web development – [javascript, html, css/scss, node, npm, webpack, vue, git, react, typescript, express, backbone, responsive]
+<br> A/B testing and KPI tracking using web analytics implementations
+<br> Analytics using IBM coremetrics
+<br> High priority/urgent code delivery and defect fixes
+<br> Unit test coverage and performance test base lining behind delivered features 
+<br> Strong emphasis on process and delivery- especially using lean principles
+<br> Web app UX research - established a/b test result implications, and proceeded with implementation 
+<br> Daily documentation of work as a steady habit
+<br> Weekly showcase of work to business stakeholders
             </p>
 <h3>Macy's Inc. Tech Star Executive Development Program San Francisco, CA Aug 2015 – Jan 2016</h3>
     <p>
-• Mobile web platform development team - [javascript,jquery, html,css/scss, handlebars, hapi, grunt]
-<br> • Contributions to Macy's mobile single page webapp
-<br> • Human resources sponsored training sessions
-<br>• Delivered monthly showcase to program stakeholders, managers, and colleagues
-<br>• Mobile Platform and Content Engineering - creation of common components for web developers, executed library updates</p>
+ Mobile web platform development team - [javascript,jquery, html,css/scss, handlebars, hapi, grunt]
+<br>  Contributions to Macy's mobile single page webapp
+<br>  Human resources sponsored training sessions
+<br> Delivered monthly showcase to program stakeholders, managers, and colleagues
+<br> Mobile Platform and Content Engineering - creation of common components for web developers, executed library updates</p>
 
 
 <h3>Autoliv – Night Vision Goleta, CA Software Intern June 2015-Aug 2015</h3>
 <p>
-• Performed validation of newly updated gain calibration software on production floor equipment  <br>
-• Wrote documentation used for the validation of software used to calibrate night vision cameras <br>
-• Installed updates of calibration software on production floor equipment<br>
-• Worked with EE lab to deliver script that transferred template files - [powershell]<br>
+ Performed validation of newly updated gain calibration software on production floor equipment  <br>
+ Wrote documentation used for the validation of software used to calibrate night vision cameras <br>
+ Installed updates of calibration software on production floor equipment<br>
+ Worked with EE lab to deliver script that transferred template files - [powershell]<br>
 </p>
 
 <h2>PROJECTS</h2>
 <p>
-• MACY'S STORE HUB: [javascript, vue, vuex, node, npm, webpack] Front End development for a Macy's internal tool that manages Macy's store event information
-<br>• MACY'S OMNICLIENT STYLIST TOOL: [ javascript, node, angular, java, jboss, maven, sql ] Full stack development for a Macy's internal webapp used by Macy's store stylists to communicate with their clients via text
-<br>• MACY’S VOICE LAB: [typescript, node, express, firebase, google cloud, aws-lambda, actions on google, alexa v2] Researched and developed voice applications for Google Home and Alexa platforms utilizing Google Cloud and AWS Lambda. 
+ <span>MACY'S STORE HUB</span>: [javascript, vue, vuex, node, npm, webpack] Front End development for a Macy's internal tool that manages Macy's store event information
+<br> <span>MACY'S OMNICLIENT STYLIST TOOL</span>: [ javascript, node, angular, java, jboss, maven, sql ] Full stack development for a Macy's internal webapp used by Macy's store stylists to communicate with their clients via text
+<br> <span>MACY’S VOICE LAB</span>: [typescript, node, express, firebase, google cloud, aws-lambda, actions on google, alexa v2] Researched and developed voice applications for Google Home and Alexa platforms utilizing Google Cloud and AWS Lambda. 
 Assisted in developing a shareable component that developers could extend in order to minimize code duplication and for fast delivery
-<br>• SHOPPABLE CONTENT LEAN LAB : [javascript, node, backbone, handlebars] Responsible for running experiments and studying behavior of Macy’s customers on the responsive website to gain insight on producing
+<br> <span>SHOPPABLE CONTENT LEAN LAB</span>: [javascript, node, backbone, handlebars] Responsible for running experiments and studying behavior of Macy’s customers on the responsive website to gain insight on producing
 valuable content recommendations
-<br>• MOBILE WEB INNOVATION LEAN LAB : [javascript, node, backbone, handlebars] Responsible for running front-end A/B experiments, studying the signals of our mobile web customers and implementing features that provide an enhanced mobile shopping experience
-<br>• GOOGLE AMP HACKATHON, NYC 2018: Macy’s homepage written in AMP
-<br>• MACY’S HACKATHON TOP 5 CONTESTANT, 2017: Thumbs up! implemented a tap and zoom capability on the mobile website’s browse and search pages for viewing a high resolution image
+<br> <span>MOBILE WEB INNOVATION LEAN LAB</span>: [javascript, node, backbone, handlebars] Responsible for running front-end A/B experiments, studying the signals of our mobile web customers and implementing features that provide an enhanced mobile shopping experience
+<br> <span>GOOGLE AMP HACKATHON, NYC 2018</span>: Macy’s homepage written in AMP
+<br> <span>MACY’S HACKATHON TOP 5 CONTESTANT, 2017</span>: Thumbs up! implemented a tap and zoom capability on the mobile website’s browse and search pages for viewing a high resolution image
 of the product
-<br>• MACY’S IDEA LAB, MEN’ S WEARHOUSE ONLINE TUXEDO RENTAL PROJECT: Developed front-end for “crawl” phase of a new partnership with Men’s Wearhouse that would ease the rental process for groups including the process for sizing and ordering
-<br>• MOBILE WEB PLATFORM : Developed code and took ownership alongside a veteran team for optimizing the Macy's website for mobile/tablet devices
+<br> <span>MACY’S IDEA LAB, MEN’ S WEARHOUSE ONLINE TUXEDO RENTAL PROJECT</span>: Developed front-end for “crawl” phase of a new partnership with Men’s Wearhouse that would ease the rental process for groups including the process for sizing and ordering
+<br> <span>MOBILE WEB PLATFORM</span>: Developed code and took ownership alongside a veteran team for optimizing the Macy's website for mobile/tablet devices
  </p>   
          </div>
          <navBar/>
@@ -82,10 +82,13 @@ export default {
 #about {
     text-align: center;
 }
+span {
+    font-weight: 550;
+}
 .bio {
     font-size: 14px;
     font-weight: 400;
-    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    font-family: 'Helvetica', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
 }
 img {
     display: block;
