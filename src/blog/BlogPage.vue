1519039b5a41916f44ee820ca3ee7160333d65de
jess cosio <jessica.cosio@icloud.com>
2022-03-04 09:38:06 -0800

    started blog page

---
+++ src/blog/BlogPage.vue
@@ -0,0 +1,54 @@
+<template>
+<div id="BlogPage">
+    <navBar />
+</div>
+    
+</template>
+
+<script>
+import navBar from '../components/navBar.vue';
+
+const largeScreen = 724;
+const smallScreen = 330;
+
+const HomePage = {
+    name: 'BlogPage',
+    components: {navBar},
+    data : function(){
+        return {
+            windowWidth: window.innerWidth
+        };
+        
+    },
+    created() {
+        window.addEventListener('resize', this.handleResize);
+        this.loadBackground();
+    },
+    beforeDestroy() {
+        window.removeEventListener('resize', this.handleResize);
+        document.body.style.overflow = ""; // -- should reset modified body -- 
+    },
+    methods: {
+        handleResize: function() {
+
+            this.windowWidth = window.innerWidth;
+        },
+
+    }
+    
+}; 
+export default BlogPage;
+</script>  
+
+<style scoped>
+
+#BlogPage {
+    position: fixed;
+    height: 100vh;
+    width: 100vw;
+    
+}
+
+   
+
+</style>
